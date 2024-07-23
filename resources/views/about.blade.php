3645e50892234decfbea8ec6f4f7a66ba46b2e85
Александр Неудахин <makewww68@Air-Aleksandr.localnet>
2024-07-23 07:38:07 +0300

    start

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>About us</x-slot:heading>
+    <h1>About page</h1>
+</x-layout>
---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Our contacts</x-slot:heading>
+    <h1>Contacts page</h1>
+</x-layout>
