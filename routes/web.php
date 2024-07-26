fe987e4509da7b8661b50eefb658858dcd81d13e
Александр Неудахин <makewww68@Air-Aleksandr.localnet>
2024-07-26 08:06:19 +0300

    db

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,21 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
-$jobs = [
-    [
-        'id' => 1,
-        'name' => 'Director',
-        'salary' => '$50 000',
-    ],
-    [
-        'id' => 2,
-        'name' => 'Programmer',
-        'salary' => '$10 000',
-    ],
-    [
-        'id' => 3,
-        'name' => 'Teacher',
-        'salary' => '$40 000',
-    ],
-];
+
 
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/jobs', function () use ($jobs) {
+Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => $jobs
+        'jobs' => Job::all()
     ]);
 });
-Route::get('/jobs/{id}', function ($id) use ($jobs) {
+Route::get('/jobs/{id}', function ($id) {
 
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
 
